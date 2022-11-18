738a2fdc0f1756304d170aea549fe873fb63c457
vikashsinghldp <89346884+vikashsinghldp@users.noreply.github.com>
2022-11-18 10:11:58 +0000

    Update settings.py

--- vsrtech/settings.py
+++ vsrtech/settings.py
@@ -115,13 +115,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    
-]
 
 STATIC_URL = 'static/'
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
