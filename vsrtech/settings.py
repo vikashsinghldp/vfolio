fea41c61fa4e9144276a9ac7f49a22dcce8ec1f2
vikashsinghldp <89346884+vikashsinghldp@users.noreply.github.com>
2022-11-18 10:04:05 +0000

    Update settings.py

--- vsrtech/settings.py
+++ vsrtech/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     
